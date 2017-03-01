b1ef1ec4762352492d7bd6397e88399d43eba6e5
Nick Gang <orangagang@gmail.com>
2017-03-01 00:20:33 -0800

    Added new method to calculate spectralenvelope of high frequency critical bands. Single value returned per critical band denoting average intensity.

--- baseCoder/SBR.py
+++ baseCoder/SBR.py
@@ -6,6 +6,32 @@ Copyright 2017 Nick Gang -- All rights reserved
 -----------------------------------------------------------------------
 """
 import numpy as np
+import window as w
+
+########## Encoder Methods ##########
+
+def calcSpecEnv(data,cutoff,fs):
+    # calcSpecEnv - Function to calculate spectral envelope passed to decoder for SBR
+    # data:      input signal for current block
+    # cutoff:    Cutoff frequency in Hz
+    # fs:        Sampling rate in Hz
+
+    N = data.size
+    freqVec = np.arange(0,fs/2,fs/float(N)) # Vector of FFT bin frequencies
+    Xn = np.fft.fft(w.HanningWindow(data),N)
+    hannWin = (1/float(N))*np.sum(np.power(w.HanningWindow(np.ones_like(data)),2)) # Get avg pow of hann window
+    XnI = (4/(np.power(N,2)*hannWin))*(np.power(np.abs(Xn),2)) # Compute values of FFT intensity
+    bandLimits = p.cbFreqLimits # Zwicker critical band upper limits
+    cutBand = np.argwhere(bandLimits>=cutoff)[0] # Next band limit above cutoff freq
+    nHfBands = len(bandLimits)-cutBand # How many bands will be reconstructed
+    specEnv = np.zeros(nHfBands-1)
+    for i in range(nHfBands-1):
+        bandLines = np.intersect1d(np.argwhere(freqVec>bandLimits[cutBand+i]),np.argwhere(freqVec<=bandLimits[cutBand+i+1]))
+        specEnv[i] = np.mean(XnI[bandLines]) # Spec Env is avg intensity in each hi-freq critical band
+
+    return specEnv
+
+########## Decoder Methods ##########
 
 # High Frequency Reconstruction
 def HiFreqRec(mdctLines,fs,cutoff):
@@ -20,7 +46,8 @@ def AddHiFreqs(mdctLines,fs,cutoff):
     nMDCT = len(mdctLines)
     cutBin = freqToBin(nMDCT,cutoff,fs)
     noiseBins = len(mdctLines[cutBin:])
-    mdctLines[cutBin:] *= np.absolute(np.random.normal(1,0.1,noiseBins)) # Add some noise to reconstructed bins
+    mdctLines[cutBin:] *= np.absolute(np.random.normal(1,0.5,noiseBins))# Add some noise to reconstructed bins
+    mdctLines[cutBin:] *= 0.1 # Hardcoding this hack until envelope is ready
     return mdctLines
 
 # Envelope Adjustment
