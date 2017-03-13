phase shift and scaling fixes

baseCoder/stereoCoding.py
@@ -30,6 +30,7 @@ def ChannelCoupling(mdct, Fs, couplingStart):
     """
     # TODO: use spectral power to compute start of coupling
     mdct = MSEncode(np.array(mdct))
+    mdct[0] = np.roll(mdct[0],len(mdct[0]/2))
     nChannels = len(mdct)
     nMdctLines = len(mdct[0]) 
     mdctLineAssign = AssignMDCTLinesFromFreqLimits(nMdctLines, Fs)
@@ -61,11 +62,11 @@ def ChannelCoupling(mdct, Fs, couplingStart):
                 couplingParams[couplingIdx] = (power_chan/power_couple)
             couplingIdx +=1
             
-    uncoupledData = np.array(uncoupledData)/2.
+    uncoupledData = np.array(uncoupledData)
     uncoupledPad = np.zeros((len(uncoupledData),nMdctLines-len(uncoupledData[0])))
     uncoupledData = np.hstack((uncoupledData,uncoupledPad))
     coupledPad = np.zeros((nMdctLines-len(coupledChannel)))
-    coupledChannel = np.hstack((coupledPad,coupledChannel))/2.
+    coupledChannel = np.hstack((coupledPad,coupledChannel))
     return uncoupledData,coupledChannel,couplingParams
 
 def ChannelDecoupling(uncoupledData,coupledChannel,couplingParams,Fs,couplingStart):
@@ -95,6 +96,7 @@ def ChannelDecoupling(uncoupledData,coupledChannel,couplingParams,Fs,couplingSta
     mdct = []
     for n in range(nChannels):
         mdct.append(list(reconstructedChannels[n]))
+    mdct[0] = np.roll(mdct[0],-len(mdct[0]/2))
     mdct = MSDecode(np.array(mdct))
     return mdct
 