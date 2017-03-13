51266a2bffb07b27321f9f776cce48bf62a70f1f
Ifueko Igbinedion <ifueko@stanford.edu>
2017-03-12 20:55:32 -0700

    adding new SBR.py

--- baseCoder/SBR.py
+++ baseCoder/SBR.py
@@ -11,22 +11,30 @@ import window as w
 
 ########## Encoder Methods ##########
 
-def calcSpecEnv(data,cutoff,fs):
+def calcSpecEnv(data,cutoff,fs,hfRecType=2):
     # calcSpecEnv - Function to calculate spectral envelope passed to decoder for SBR
     # data:      input signal for current block
     # cutoff:    Cutoff frequency in Hz
     # fs:        Sampling rate in Hz
-
+    # hfRecType: 1 for full subband, 2 for altered subband that repeats top half
     N = data.size
     freqVec = np.arange(0,fs/2,fs/float(N)) # Vector of FFT bin frequencies
     Xn = np.fft.fft(w.HanningWindow(data),N)
     hannWin = (1/float(N))*np.sum(np.power(w.HanningWindow(np.ones_like(data)),2)) # Get avg pow of hann window
     XnI = (4/(np.power(N,2)*hannWin))*(np.power(np.abs(Xn),2)) # Compute values of FFT intensity
+    # Transpose down top half of subband if we're using altered alg
+    if hfRecType==2:
+        cutBin = freqToBinFFT(N,cutoff,fs)
+        try: # Being hacky to account for off by 1 errors
+            XnI[0:int(np.floor(cutBin/2))] = XnI[int(np.floor(cutBin/2)):cutBin]
+        except ValueError:
+            XnI[0:int(np.floor(cutBin/2))] = XnI[int(np.floor(cutBin/2)+1):cutBin]
 
     bandLimits = p.cbFreqLimits # Zwicker critical band upper limits
     cutBand = np.argwhere(bandLimits>=cutoff)[0] # Next band limit above cutoff freq
     nHfBands = len(bandLimits)-cutBand # How many bands will be reconstructed
     specEnv = np.zeros(nHfBands-1)
+
     for i in range(nHfBands-1):
         bandLines = np.intersect1d(np.argwhere(freqVec>bandLimits[cutBand+i]),\
                                    np.argwhere(freqVec<=bandLimits[cutBand+i+1]))
@@ -46,6 +54,18 @@ def HiFreqRec(mdctLines,fs,cutoff):
     nMDCT = len(mdctLines)
     cutBin = freqToBin(nMDCT,cutoff,fs)
     lowerBand = mdctLines[0:cutBin]
+    mdctLines[cutBin+1:cutBin+len(lowerBand)+1] = lowerBand # Do the transposition
+    return mdctLines.astype(float) # If these are ints it can cause problems
+
+# Alternate function, replicates top half of subband twice
+def HiFreqRec2(mdctLines,fs,cutoff):
+    nMDCT = len(mdctLines)
+    cutBin = freqToBin(nMDCT,cutoff,fs)
+    lowerBand = np.array(mdctLines[0:cutBin],copy=True)
+    try: # Being hacky to account for off by 1 errors
+        lowerBand[0:int(np.floor(cutBin/2))] = lowerBand[int(np.floor(cutBin/2)):cutBin]
+    except ValueError:
+        lowerBand[0:int(np.floor(cutBin/2))] = lowerBand[int(np.floor(cutBin/2)+1):cutBin]
     mdctLines[cutBin+1:cutBin+len(lowerBand)+1] = lowerBand # Do the transposition
     return mdctLines.astype(float) # If these are ints it can cause problems
 
@@ -80,6 +100,13 @@ def freqToBin(nMDCT,cutoff,fs):
     N = 2*nMDCT
     freqVec = np.arange(0,fs/2,fs/float(N))+fs/float(2.*N) # MDCT Frequencies
     cutBin = np.argmin(np.absolute(freqVec-cutoff)) # Find index of cutoff frequency
+    return cutBin
+
+# Utility Function to Convert Cutoff Freq to FFT Bin Number
+def freqToBinFFT(nFFT,cutoff,fs):
+    N = nFFT
+    freqVec = np.arange(0,fs/2,fs/float(N)) # FFT freq vector
+    cutBin = np.argmin(np.absolute(freqVec-cutoff))
     return cutBin
 
 # Utility Function to Convert cutoff frequency to critical band index
